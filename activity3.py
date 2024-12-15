2ac45aefbcccfc2832c03ee2c0280f630150d1bc
Amadou-Jammeh <amadoujammeh250@gmail.com>
2024-12-15 11:19:18 +0000

    activity3

---
+++ activity3.py
@@ -0,0 +1,9 @@
+num=int(input("enter a umber"))
+if num>50:
+    print(num,"is greater than 50")
+    if (num%2==0):
+        print("and the number is even")
+    else:
+        print("and the number is odd")
+else:
+     print(num,"is less than 50")
